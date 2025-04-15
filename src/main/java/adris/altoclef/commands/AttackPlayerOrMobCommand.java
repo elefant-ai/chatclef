825fa53968b7868a3ae88737a75ed8f8dae219e0
Adrisj6 <adris@elefant.gg>
2025-04-15 14:42:42 -0400

    (add mob death mixin check that validates we actually killed a mob)

--- src/main/java/adris/altoclef/commands/AttackPlayerOrMobCommand.java
+++ src/main/java/adris/altoclef/commands/AttackPlayerOrMobCommand.java
@@ -1,14 +1,21 @@
 package adris.altoclef.commands;
 
+import java.util.function.Predicate;
+
 import adris.altoclef.AltoClef;
 import adris.altoclef.commandsystem.Arg;
 import adris.altoclef.commandsystem.ArgParser;
 import adris.altoclef.commandsystem.Command;
 import adris.altoclef.commandsystem.CommandException;
+import adris.altoclef.eventbus.EventBus;
+import adris.altoclef.eventbus.Subscription;
+import adris.altoclef.eventbus.events.EntityDeathEvent;
 import adris.altoclef.tasks.ResourceTask;
 import adris.altoclef.tasks.entity.KillEntitiesTask;
 import adris.altoclef.tasksystem.Task;
 import adris.altoclef.util.ItemTarget;
+import adris.altoclef.util.time.TimerGame;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class AttackPlayerOrMobCommand extends Command {
@@ -35,6 +42,12 @@ public class AttackPlayerOrMobCommand extends Command {
 
         private int _mobKillTargetCount;
 
+        private TimerGame _forceCollectTimer = new TimerGame(2);
+
+        private Subscription<EntityDeathEvent> _onMobDied;
+
+        private Predicate<Entity> _shouldAttackPredicate;
+
         private static ItemTarget[] drops = new ItemTarget[]{
             new ItemTarget("rotten_flesh", 9999),
             new ItemTarget("bone", 9999),
@@ -50,6 +63,11 @@ public class AttackPlayerOrMobCommand extends Command {
             new ItemTarget("speckled_melon", 9999),
             new ItemTarget("gold_nugget", 9999),
             new ItemTarget("iron_nugget", 9999),
+            new ItemTarget("porkchop", 9999),
+            new ItemTarget("beef", 9999),
+            new ItemTarget("chicken", 9999),
+            new ItemTarget("mutton", 9999),
+            new ItemTarget("rabbit", 9999),
     };
 
         public AttackAndGetDropsTask(String toKill, int killCount) {
@@ -57,8 +75,12 @@ public class AttackPlayerOrMobCommand extends Command {
             _toKill = toKill;
             _mobKillTargetCount = killCount;
 
-            // Kill any entity matches our name, or if it's a player their username.
-            _killTask = new KillEntitiesTask(entity -> {
+            _shouldAttackPredicate = (entity) -> {
+                // Done, don't attack any mobs just collect
+                if (_mobsKilledCount >= _mobKillTargetCount) {
+                    return false;
+                }
+
                 String name = entity.getType().getUntranslatedName();
                 if (entity instanceof PlayerEntity) {
                     String playerName = entity.getName().getString();
@@ -67,34 +89,60 @@ public class AttackPlayerOrMobCommand extends Command {
                     }
                 }
                 return name != null && name.equals(_toKill);
-            });// new KillEntitiesTask(shouldKill, _toKill);
+            };
+
+            // Kill any entity matches our name, or if it's a player their username.
+            _killTask = new KillEntitiesTask(_shouldAttackPredicate);// new KillEntitiesTask(shouldKill, _toKill);
         }
-        
+
         @Override
         protected boolean shouldAvoidPickingUp(AltoClef mod) {
             return false;
         }
-    
+
         @Override
         protected void onResourceStart(AltoClef mod) {
-            // _mobsKilledCount = 0;
-            // TODO: Eventbus subscribe to mob killed event
+            _forceCollectTimer.reset();
+            _onMobDied = EventBus.subscribe(EntityDeathEvent.class, evt -> {
+                Entity diedEntity = evt.entity;
+                if (evt.damageSource == null || evt.damageSource.getAttacker() == null) {
+                    // System.out.println("no damage source attacker");
+                    return;
+                }
+                if (evt.damageSource.getAttacker() instanceof PlayerEntity player) {
+                    if (player.getName().getString().equals(mod.getPlayer().getName().getString())) {
+                        if (_shouldAttackPredicate.test(diedEntity)) {
+                            // System.out.println("Mob KILLED!");
+                            _mobsKilledCount++;
+                        } else {
+                            // System.out.println("predicate failed");
+                        }
+                    } else {
+                        // System.out.println("FAIL B: " + player.getName().getString() + " != " + mod.getPlayer().getName().getString());
+                    }
+                } else {
+                    // System.out.println("not player instance: " + evt.damageSource.getAttacker().getClass().getSimpleName());
+                }
+            });
         }
 
         @Override
         public boolean isFinished() {
-            // We've killed enough of the mobs
-            return _mobsKilledCount > _mobKillTargetCount;
+            // We've killed enough of the mobs AND our timer has gone...
+            return _mobsKilledCount >= _mobKillTargetCount && _forceCollectTimer.elapsed();
         }
-    
+
         @Override
         protected Task onResourceTick(AltoClef mod) {
+            if (_mobsKilledCount < _mobKillTargetCount) {
+                _forceCollectTimer.reset();
+            }
             return _killTask;
         }
-    
+
         @Override
         protected void onResourceStop(AltoClef mod, Task interruptTask) {
-                // TODO: Eventbus unsubscribe to mob killed event
+            EventBus.unsubscribe(_onMobDied);
         }
     
         @Override
