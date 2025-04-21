a3ceaf4e78f23a99bc4c9ab4db316617d2c0f8cb
Adrisj6 <adris@elefant.gg>
2025-04-21 14:16:13 -0400

    Make commands consider current item counts to work w/ agent

--- src/main/java/adris/altoclef/commands/MeatCommand.java
+++ src/main/java/adris/altoclef/commands/MeatCommand.java
@@ -7,6 +7,8 @@ import adris.altoclef.commandsystem.Command;
 import adris.altoclef.commandsystem.CommandException;
 import adris.altoclef.tasks.resources.CollectMeatTask;
 
+import adris.altoclef.util.helpers.ItemHelper;
+
 public class MeatCommand extends Command {
     public MeatCommand() throws CommandException {
         super("meat", "Collects a certain amount of meat", new Arg<>(Integer.class, "count"));
@@ -14,6 +16,10 @@ public class MeatCommand extends Command {
 
     @Override
     protected void call(AltoClef mod, ArgParser parser) throws CommandException {
-        mod.runUserTask(new CollectMeatTask(parser.get(Integer.class)), this::finish);
+        int count = parser.get(Integer.class);
+        // agent integration
+        count += AltoClef.getInstance().getItemStorage().getItemCountInventoryOnly(ItemHelper.COOKED_FOODS );
+
+        mod.runUserTask(new CollectMeatTask(count), this::finish);
     }
 }
